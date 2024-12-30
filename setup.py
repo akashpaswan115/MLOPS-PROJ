5764991a600647599baf63facddacde8e7696f64
Akash <akashpaswan.iitkgp@gmail.com>
2024-12-31 03:49:58 +0530

    project tsructure, ready venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Akash Paswan",
+    author_email="akashpaswan.iitkgp@gmail.com",
+    packages=find_packages()
+)
